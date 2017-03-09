324b1a4e91ac12ad55cc95836abaf02631e8d6f7
Cody Buffaloe <codylbuffaloe@gmail.com>
2017-03-08 18:06:38 -0800

    Still haven't gotten the hang of splitting the array

--- Merge_Sort.rb
+++ Merge_Sort.rb
@@ -1,27 +1,10 @@
 def merge_sort(unsorted, a =[], b = [])
-  if(unsorted.size() > 1)
-    sorted_list = []
-    unsorted.each_with_index do |el, ind|
-      if(ind % 2 == 0)
-        b << el
-      else
-        a << el
-      end
-    end
-    puts "b"
-    puts b
-    puts "a"
-    puts a
-    if(a.first() > b.first())
-      sorted_list << b.first()
-    else
-      sorted_list << a.first()
-    end
-    
-    puts sorted_list
-  else
+  if(unsorted.size() == 1)
     return unsorted
+  else
+
+
   end
 end
 
-puts merge_sort([8, 7, 10])
+puts merge_sort([8, 12])
