e2e8fd432c9e90877655fc086611523743517a03
Cody Buffaloe <codylbuffaloe@gmail.com>
2017-03-21 13:55:38 -0700

    Completed project

--- Merge_Sort.rb
+++ Merge_Sort.rb
@@ -8,14 +8,10 @@ end
 
 def actual_merge(left_unsorted, right_unsorted)
   sorted = []
-  puts "Left"
-  puts left_unsorted
-  puts "Right"
-  puts right_unsorted
-  until( (left_unsorted.length == 0) || (right_unsorted.length == 0) ) do
+  until((left_unsorted.length == 0) || (right_unsorted.length == 0)) do
     sorted << (left_unsorted.first <= right_unsorted.first ? left_unsorted.shift : right_unsorted.shift)
   end
   sorted + left_unsorted + right_unsorted
 end
 
-puts mergesort([8, 12, 3, 4, 56, 78, 9, 12, 13, 45, 6, 76])
+puts mergesort([8, 12, 3, 4, 56, 78, 9, 12, 13, 45, 6, 76, 96, 14, 11, 98])
